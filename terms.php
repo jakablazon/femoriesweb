112fba45839fa724d67fbc59cb4219dc1da51bef
jakablazon <jakablazon@Jakas-MacBook-Pro.local>
2016-04-18 22:18:30 -0700

    Added privacy policy

---
+++ terms.php
@@ -0,0 +1,30 @@
+	<section id="page-top" class="section-style" data-background-image="images/background/page-top.jpg">
+		<div class="pattern height-resize">
+			<div class="container">
+				<h1 class="site-title">
+					Femories
+				</h1><!-- /.site-title -->
+				<h3 class="section-name">
+					<span>
+					</span>
+				</h3><!-- /.section-name -->
+				<h2 class="section-title">
+					Terms and conditions
+				</h2><!-- /.Section-title  -->
+				<p> Here there will be Terms and conditions </p>
+			</div><!-- /.container -->
+		</div><!-- /.pattern -->		
+	</section><!-- /#page-top -->
+	
+			<!-- Footer Section -->
+		<footer id="footer-section">
+			<p class="copyright">
+			 2016, All Rights Reserved.</a>
+			</p>
+			<p class="copyright">
+            
+             2016, All Rights Reserved. <a href="/privacy.php">Privacy Policy</a>
+            
+            </p> 
+		</footer>
+		<!-- Footer Section End -->
